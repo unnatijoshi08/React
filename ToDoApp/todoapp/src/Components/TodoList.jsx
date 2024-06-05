68899216a499ecdba6d6c0f850349acb082a1393
Unnati <unnati.ht344@gmail.com>
2024-06-05 19:53:16 +0530

    add empty list componenet

--- ToDoApp/todoapp/src/Components/TodoList.jsx
+++ ToDoApp/todoapp/src/Components/TodoList.jsx
@@ -4,7 +4,12 @@ export default function TodoList({ todos, setTodos }) {
   const sortedTodos = todos
     .slice()
     .sort((a, b) => Number(a.done) - Number(b.done));
-  return (
+  const length = sortedTodos.length;
+  return length === 0 ? (
+    <div className={styles.noList}>
+      <h2>Hey, your todo list seems empty....</h2>
+    </div>
+  ) : (
     <div className={styles.list}>
       {sortedTodos.map((item) => (
         <Todoitem
